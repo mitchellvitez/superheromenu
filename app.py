ef9e65a96e46a7791f555154ca43f56f8468c211
Mitchell Vitez <mitchellvitez@wf106-192.ust.hk>
2015-06-24 07:25:25 +0800

    Basic login functionality and User model

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, url_for
 from flask.ext.pymongo import PyMongo
 from flask.ext.login import LoginManager, login_required, login_user, logout_user, current_user
+import datetime
 
 """
 Flask for login/security issues, api
@@ -11,25 +12,49 @@ app = Flask(__name__)
 
 # set up database
 mongo = PyMongo(app)
+with app.app_context():
+	db = mongo.db
 
 # set up user logins
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+class User():
+	def __init__(self, username, password, email):
+		self.username = username
+		self.password = password
+		self.email = email
+
+	def is_authenticated(self):
+		return True
+
+	def is_active(self):
+		return True
+
+	def is_anonymous(self):
+		return False
+
+	def get_id(self):
+		return unicode(self.username)
+
 @login_manager.user_loader
 def load_user(username):
-	return mongo.db.users.find_one({'username': username})
+	# returns user object, sans password, from this user id
+	# return db.users.find_one({'username': username}, {'password': False})
+	return User('mitchellvitez', 'testpass', 'mitchellvitez@gmail.com')
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    return "redirect(url_for('index'))"
+	login_user(User('mitchellvitez', 'testpass', 'mitchellvitez@gmail.com'))
+	return "logged in %s" % str(current_user.username)
 
 @app.route('/testdb')
+@login_required
 def testDatabase():
-	mongo.db.users.remove() # remove all users
-	mongo.db.users.save({'username':'mitchellvitez', 'online':True})
+	# db.users.remove() # remove all users
+	# db.users.save({'username':'mitchellvitez', 'online':True})
 	# return str( mongo.db.users.find_one() )
-	return 'users online: %s' % mongo.db.users.find_one({'online': True})
+	return 'users: %s' % db.users.find_one()
 
 @app.route('/api/<restaurantName>/search/<query>')
 def search(restaurantName, query):
