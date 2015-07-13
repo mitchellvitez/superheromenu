918914f36fd87d0883d3f65021c90df30b3b32f6
Mitchell Vitez <mitchell@vitez.me>
2015-07-13 18:33:43 +0800

    Fixed syntax

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ with open("app.py", "rt") as fin:
             
 with open("app.py", "wt") as fout:
 	s = s.replace('app.run(debug=True)', 'app.run()')
-	s = s.replace("@app.route('/admin/resetdb')", "# @app.route('/admin/resetdb')"))
+	s = s.replace("@app.route('/admin/resetdb')", "# @app.route('/admin/resetdb')")
 	fout.write(s)
