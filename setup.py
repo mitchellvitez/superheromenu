1a8280a681a235073743db74a9164f5a551b5a7d
Mitchell Vitez <mitchell@vitez.me>
2015-07-13 18:32:03 +0800

    Improved setup script

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 
+os.system('git stash')
 os.system('git pull')
 
 s = ''
@@ -9,4 +10,6 @@ with open("app.py", "rt") as fin:
 		s += line
             
 with open("app.py", "wt") as fout:
-	fout.write(s.replace('app.run(debug=True)', 'app.run()').replace("@app.route('/admin/resetdb')", "# @app.route('/admin/resetdb')"))
+	s = s.replace('app.run(debug=True)', 'app.run()')
+	s = s.replace("@app.route('/admin/resetdb')", "# @app.route('/admin/resetdb')"))
+	fout.write(s)
